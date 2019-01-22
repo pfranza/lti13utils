d540e1f5950b2a3ecaefca1a4a391613a6c2d956
peter <peter@42lines.net>
2019-01-22 08:45:39 -0500

    initial project setup

---
+++ src/main/java/com/peterfranza/ltiutils/oidc/LTIOIDCResponseFactory.java
@@ -0,0 +1,23 @@
+package com.peterfranza.ltiutils.oidc;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class LTIOIDCResponseFactory {
+
+	private HttpServletRequest request;
+
+	public LTIOIDCResponseFactory(HttpServletRequest request) {
+		this.request = request;
+	}
+	
+	public boolean isValidRequest() {
+		return true;
+	}
+	
+	public String generateReponseURL() {
+		return null;
+	}
+	
+	
+	
+}
